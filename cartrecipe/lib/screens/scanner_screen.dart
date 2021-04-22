cdb2ca3477704cab2d193b819e7a0f3c5787ea6d
Juan David Ramírez Girón <33266718+jdmac94@users.noreply.github.com>
2021-04-22 13:02:06 +0200

    Testing Escaner
    Pruebas varias con el escáner

--- cartrecipe/lib/screens/scanner_screen.dart
+++ cartrecipe/lib/screens/scanner_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:openfoodfacts/openfoodfacts.dart';
+import 'package:flutter/services.dart';
 
 import 'dart:async';
 
@@ -33,8 +34,41 @@ class _ScannerScreenState extends State<ScannerScreen> {
     }
   }
 
+  Future<String> scanBarcodeNormal() async {
+    String barcodeScanRes;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', 'Cancel', true, ScanMode.BARCODE);
+      print(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    //if (!mounted) return;
+
+    setState(() {
+      _data = barcodeScanRes;
+    });
+
+    return _data;
+  }
+
   @override
   Widget build(BuildContext context) {
+    //   return FutureBuilder<String>(
+    //       future: _scan(),
+    //       builder: (context, AsyncSnapshot<String> snapshot) {
+    //         if (snapshot.hasData) {
+    //           return Text(snapshot.data);
+    //         } else {
+    //           return CircularProgressIndicator();
+    //         }
+    //       });
+    // }
     return Scaffold(
       body: Center(
         child: Column(
@@ -44,9 +78,6 @@ class _ScannerScreenState extends State<ScannerScreen> {
                 onPressed: () => _scan(),
                 child: Text("Escanear código de barras")),
             Text(_data),
-            ElevatedButton(
-                onPressed: () => getProduct(), child: Text("Prueba producto")),
-            Text(_productname),
           ],
         ),
       ),
