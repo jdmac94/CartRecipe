37d3ca2881e6c565dab2b3919899af4ba8779305
Andrés Bitto <andresjavier.bitto@e-campus.uab.cat>
2021-04-22 19:27:14 +0200

    new route for testing porpouses

--- Backend/routes/nevera.js
+++ Backend/routes/nevera.js
@@ -1,12 +1,41 @@
 const express = require("express");
 const https = require("https");
 const router = express.Router();
+const fetch = require('node-fetch');
 const { Nevera } = require("../models/nevera");
 const { Product } = require("../models/product");
 let OFFurl = 'https://world.openfoodfacts.org/api/v0/product/';
 
 
-router.get("/getProdbyAPI", async (req, res) => {
+router.post("/apiImg", async (req, res) => {
+
+    let url = OFFurl + req.body.barcode + '.json';
+
+    let fotos;
+    // fetch(url)
+    //     .then(res => res.json())
+    //     .then(json => {
+    //         var x = json.product.selected_images.front.display;
+    //         console.log(x);
+    //     });
+        //fotos = json.product.selected_images.front);
+
+        fetch(url)
+        //.then(res => res.json())
+        .then(function(response) {
+            return response.json();
+        })
+        .then(json => {
+            var x = json.product.selected_images.front.display;
+            console.log(x[0]);
+            res.send(x);
+        });
+
+    
+    res.send(fotos);
+});
+
+router.post("/getProdbyAPI", async (req, res) => {
 
     //let producto = await Product.find( { _id: req.body.barcode } );
     let url = OFFurl + req.body.barcode + '.json';
@@ -78,6 +107,7 @@ router.post("/getProd", async (req, res) => {
 
 router.get("/getNevera", async (req, res) => {
 
+    console.log("GETTING NEVERA");
     // let nevera = await Nevera.find( { usuario: req.body.user } )
     let nevera = await Nevera.findOne();
 
@@ -93,12 +123,66 @@ router.get("/getNevera", async (req, res) => {
     
 
     let prodArray = nevera.productos;
-    let listedPords = await Product.find( { _id: { $in: prodArray } }, { _keywords:1, allergens_from_user, product_name_es, nutriscore_data, nova_group} );
+    let listedPords = await Product.find( { _id: { $in: prodArray } }, { _keywords:1, allergens_from_user:1, product_name_es:1, nutriscore_data:1, nova_group:1} );
 
     res.send(listedPords);
 
 });
 
+
+router.get("/getNeveraList", async (req, res) => {
+
+    console.log("GETTING NEVERA LIST"+ Date.now());
+    // let nevera = await Nevera.find( { usuario: req.body.user } )
+    let nevera = await Nevera.findOne();
+
+    //test
+    if (!nevera) {
+        nevera = new Nevera();
+        nevera.usuario = "0";
+        nevera.productos = ["0000000018449", "5449000000996"];
+        //nevera.usuario = req.body.user;
+        //nevera.productos = [];
+        const result = nevera.save();
+    }
+    
+
+    let prodArray = nevera.productos;
+    let listedPords = await Product.find( { _id: { $in: prodArray } }, { product_name:1, allergens_from_user:1, product_name_es:1,} );
+
+    res.send(listedPords);
+
+});
+
+router.get("/getNeveraListIMG", async (req, res) => {
+
+    console.log("GETTING NEVERA LIST");
+    // let nevera = await Nevera.find( { usuario: req.body.user } )
+    let nevera = await Nevera.findOne();
+
+    //test
+    if (!nevera) {
+        nevera = new Nevera();
+        nevera.usuario = "0";
+        nevera.productos = ["5449000000996"];
+        //nevera.usuario = req.body.user;
+        //nevera.productos = [];
+        const result = nevera.save();
+    }
+    
+
+    let prodArray = nevera.productos;
+    let listedPords = await Product.find( { _id: { $in: prodArray } }, { product_name:1, allergens_from_user:1, product_name_es:1,} );
+
+    let img = "https://static.openfoodfacts.org/images/products/073/762/806/4502/front_en.6.200.jpg";
+
+    listedPords.forEach(function(element) {
+        element.imgs = [ img ];
+    });
+
+    res.send(listedPords);
+
+});
 
 router.get("/getNeveraArray", async (req, res) => {
 
@@ -195,6 +279,22 @@ router.put("/addNevera", async (req, res) => {
             res.send(nevera.productos);
     } else
         return res.status(400).send("El Producto ya exsite en la nevera");
+});
+
+
+router.get("/restoreNevera", async (req, res) => {
+
+    console.log("GETTING NEVERA LIST");
+    // let nevera = await Nevera.find( { usuario: req.body.user } )
+    let nevera = await Nevera.remove( { } );
+
+    nevera = new Nevera();
+    nevera.usuario = "0";
+    nevera.productos = ["5449000000996"];
+    //nevera.usuario = req.body.user;
+    //nevera.productos = [];
+    const result = nevera.save();
+
 });
 
 module.exports = router;
