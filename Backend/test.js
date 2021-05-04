1f775c1b3944513fc34d3e65d76e321b1a853afb
Andrés Bitto <andresjavier.bitto@e-campus.uab.cat>
2021-05-04 22:44:15 +0200

    testing category translation system

---
+++ Backend/test.js
@@ -0,0 +1,55 @@
+var fs = require('fs');
+
+var input = fs.createReadStream('aaa.txt');
+
+var rl  = require('readline').createInterface({
+    input: input,
+    terminal: false
+});
+
+var cats = "Напитки, Газированные напитки, Газированные безалкагольные напитки, en:Colas, Напитки с добавлением сахара"
+
+var str = cats.split(",");
+console.log(str[0]);
+
+for (let element of str) {
+    element = element.trim();
+}
+var temp = [];
+var catching = false;
+
+rl.on('line', function(line){
+
+    if (line.includes("<")) {
+        catching = true;
+    }
+
+    
+    //revisa el array recogido
+    if (catching) {
+
+        if (line == "\n") {
+            matches = false;
+
+            for (let element of temp) {
+                if (element.includes("varias")) {
+                    matches = true;
+                }
+            }
+
+            if (matches) {
+                console.log(temp);
+                const x = fs.readFileSync('message.txt');
+                console.log(x);
+            }
+                
+
+        } else  {
+            fs.appendFileSync('message.txt', line);
+            temp.push(line);
+        }
+    }
+
+
+
+});
