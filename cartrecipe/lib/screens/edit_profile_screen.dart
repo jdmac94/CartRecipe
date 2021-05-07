f8ca3e5607e3f1bc28b1c7c9312278c3444c2b5c
Ruaal <rualjll@gmail.com>
2021-05-07 20:26:55 +0200

    Add Edit_Profile_Screen first version

---
+++ cartrecipe/lib/screens/edit_profile_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class EditProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            child: ListView(
+      children: [
+        SizedBox(
+          height: 30,
+        ),
+        Container(
+          width: 130,
+          height: 130,
+          decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                  image: NetworkImage(
+                      "https://cdn.icon-icons.com/icons2/1863/PNG/512/account-circle_119476.png"))),
+        ),
+        TextField(
+          decoration: InputDecoration(
+            labelText: "Nom",
+          ),
+        ),
+        TextField(
+          decoration: InputDecoration(
+            labelText: "Email",
+          ),
+        ),
+        TextField(
+          decoration: InputDecoration(
+            labelText: "Contraseña",
+          ),
+        ),
+      ],
+    )));
+  }
+}
