b24bae298cc943c4b89266facb8bb55d5e12253e
Andrés Bitto <andresjavier.bitto@e-campus.uab.cat>
2021-05-22 17:52:37 +0200

    Modificación en los campos almacenados en productV2

--- Backend/routes/productV2.js
+++ Backend/routes/productV2.js
@@ -31,12 +31,28 @@ router.post("/", async (req, res) => {
           imgs: "https://www.blackwallst.directory/images/NoImageAvailable.png",
         },
         {
-          id,
-          name: data.product.product_name_es,
-          allergens_tags: data.product.allergens_tags,
-          nutriscore_data: data.product.nutriscore_data,
-          nova_group: data.product.nova_group,
-          imgs: data.product.selected_images.front.display,
+          // id,
+          // name: data.product.product_name_es,
+          // allergens_tags: data.product.allergens_tags,
+          // nutriscore_data: data.product.nutriscore_data,
+          // nova_group: data.product.nova_group,
+          // imgs: data.product.selected_images.front.display,
+            id,
+            product_name : data.product.product_name,
+            product_name_es : data.product.product_name_es,
+            nutriments : data.product.nutriments,
+            ecoscore_grade : data.product.ecoscore_grade,
+            nova_groups : data.product.nova_groups,
+            quantity : data.product.quantity,
+            nutriscore_grade : data.product.nutriscore_grade,
+            ingredients_analysis_tags : data.product.ingredients_analysis_tags,
+            allergens_tags : data.product.allergens_tags,
+            traces : data.product.traces,
+            traces_tags : data.product.traces_tags,
+            ingredients_text_es : data.product.ingredients_text_es,
+
+            imgs: data.product.selected_images.front.display,
+
         },
       ],
     });
@@ -50,27 +66,33 @@ router.post("/", async (req, res) => {
         $push: {
           products: {
             id,
+            product_name : data.product.product_name,
+            product_name_es : data.product.product_name_es,
+            nutriments : data.product.nutriments,
+            ecoscore_grade : data.product.ecoscore_grade,
+            nova_groups : data.product.nova_groups,
+            quantity : data.product.quantity,
+            nutriscore_grade : data.product.nutriscore_grade,
+            ingredients_analysis_tags : data.product.ingredients_analysis_tags,
+            allergens_tags : data.product.allergens_tags,
+            traces : data.product.traces,
+            traces_tags : data.product.traces_tags,
+            ingredients_text_es : data.product.ingredients_text_es,
+
+            imgs: data.product.selected_images.front.display,
+
+            /* 
+            id,
             name: data.product.product_name_es,
             allergens_tags: data.product.allergens_tags,
             nutriscore_data: data.product.nutriscore_data,
             nova_group: data.product.nova_group,
             imgs: data.product.selected_images.front.display,
+            */
+
             ////////////////////////////////////////////////////////
             // hablar con front para saber que campos usan 100% y evitar almacenar info innecesaria
             // ahora mismo rtabajan con estos datos, pero alguno sobra: 
-            // product_name: 1,
-            // product_name_es: 1,
-            // nutriments: 1,
-            // ecoscore_grade: 1,
-            // nova_groups: 1,
-            // quantity: 1,
-            // nutriscore_grade: 1,
-            // // ingredients: 1,
-            // ingredients_analysis_tags: 1,
-            // allergens_tags: 1,
-            // traces: 1,
-            // traces_tags: 1,
-            // ingredients_text_es: 1,
           },
         },
       }
@@ -81,7 +103,7 @@ router.post("/", async (req, res) => {
   }
 });
 
-
+//revisar en base al cambio de campos
 async function fetchAllergens(product) {
   
     // var test = "Nuez natural";
@@ -102,9 +124,8 @@ async function fetchAllergens(product) {
   
     x = allergens[0].allerg[0];
     console.log(allergens[0].allerg[0]);
-  
-    const filteredArray = 
-    arr1 = x[0]
+
+    arr1 = x[0];
   
     for (let i = 1; i < x.length; i++) {
       arr1 = arr1.filter(value => x[i].includes(value));
@@ -121,6 +142,53 @@ async function fetchAllergens(product) {
     res.send(arr1);
 }
 
+async function fetchDiet(product) {
+  
+  // var test = "Nuez natural";
+  
+  // const allergens = await ProductV2.aggregate([
+  //   { $match: { name: product } },
+  //   {
+  //     $group:
+  //       {
+  //         _id: "a",
+  //         allerg: { $push: "$products.allergens_tags" },
+  //       }
+  //   },
+  //   { $project: { name: 1, allerg: 1} }
+  // ]).exec();
+
+  const allergens = await ProductV2.aggregate([
+    { $match: { name: product } },
+    {
+      $group:
+        {
+          _id: "a",
+          allerg: { $push: "$products.ingredients_analysis_tags" },
+        }
+    },
+    { $project: { name: 1, allerg: 1} }
+  ]);
+
+  // x = allergens[0].allerg[0];
+  // console.log(x);
+
+  // arr1 = x[0];
+
+  // for (let i = 1; i < x.length; i++) {
+  //   arr1 = arr1.filter(value => x[i].includes(value));
+  // }
+
+  console.log(allergens);
+
+  // const genericProd = await ProductV2.find({ name : test });
+
+  // genericProd.allergens_from_user = arr1;
+
+  // genericProd.save();
+
+  res.send(allergens);
+}
 
 
 module.exports = router;
